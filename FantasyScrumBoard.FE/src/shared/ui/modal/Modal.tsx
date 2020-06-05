1b502e281832ede7bd19d174067d523f69764bb3
polubis <polubis@wp.pl>
2020-06-06 01:48:59 +0200

    Add option to adding project list

--- FantasyScrumBoard.FE/src/shared/ui/modal/Modal.tsx
+++ FantasyScrumBoard.FE/src/shared/ui/modal/Modal.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 
+import CloseIcon from '@material-ui/icons/Close';
+
 import { usePortal } from 'shared/utils';
+
+import { Button } from 'shared/ui';
 
 import csx from './Modal.scss';
 
 interface ModalProps {
   children: React.ReactNode;
+  onClose(): void;
 }
 
-export const Modal = ({ children }: ModalProps) => {
+export const Modal = ({ children, onClose }: ModalProps) => {
   const render = usePortal();
 
-  return render(<div className={csx.modal}>{children}</div>);
+  return render(
+    <div className={csx.modal} onClick={close}>
+      <Button variant="icon" onClick={onClose}>
+        <CloseIcon />
+      </Button>
+      {children}
+    </div>
+  );
 };

e2f2fcbd2498cd82a6a36022f88232e13b67d35a
Fabian Domurad <fabian.domurad@billennium.pl>
2020-06-06 18:25:34 +0200

    mid work

---
+++ FantasyScrumBoard.BE/FantasyScrumBoard.BE.Shared/ViewModels/SprintDetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using FantasyScrumBoard.BE.Shared.Dto;
+using FantasyScrumBoard.BE.Shared.Models;
+
+namespace FantasyScrumBoard.BE.Shared.ViewModels
+{
+    public class SprintDetailsViewModel
+    {
+        public long Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EstimatedEndDate { get; set; }
+        public DateTime? ClosedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int Number { get; set; }
+
+        public int Mvp { get; set; }
+        public IEnumerable<WorkItem> WorkItems { get; set; }
+    }
+}
